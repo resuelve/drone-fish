ab8ba6c1dbf261e72d72a30e936394d5561b0f96
Francisco Flores <franciscocapipo@gmail.com>
2021-05-24 21:43:29 -0700

    Mueve todo dentro del card

--- post-message.sh
+++ post-message.sh
@@ -9,28 +9,55 @@ elif [ ! -z ${PLUGIN_WEBHOOK} ]; then
 fi
 
 if [ "${DRONE_BUILD_STATUS}" == 'failure' ]; then
-	EVENT_MESS="*Uh ...*"
+	EVENT_MESS="Uh ..."
 	EVENT_STATUS=https://raw.githubusercontent.com/resuelve/drone-fish/master/images/ups.png
 else
 	if [ "${DRONE_BUILD_EVENT}" == 'pull_request' ]; then
-		EVENT_MESS="*Por favor CR...*"
+		EVENT_MESS="Por favor CR..."
 		EVENT_STATUS=https://raw.githubusercontent.com/resuelve/drone-fish/master/images/please.png
     else
-		EVENT_MESS="*Listo ...*"
+		EVENT_MESS="Listo ..."
 		EVENT_STATUS=https://raw.githubusercontent.com/resuelve/drone-fish/master/images/success.png
 	fi
 fi
 
 data='{
-  "text":" *Drone:* '"${DRONE_REPO}"' '"${DRONE_BUILD_LINK}"'
-  *Commit:* '"${DRONE_COMMIT_REF}"': '"${DRONE_COMMIT_MESSAGE}"'
-  *Author:* '"${DRONE_COMMIT_AUTHOR}"'
-  *Event #:* '"${DRONE_BUILD_NUMBER}"': '"${DRONE_BUILD_EVENT}"'
-  '"${EVENT_MESS}"'",
   "cards": [
     {
+      "header": {
+        "title": "'"${DRONE_REPO}"'",
+        "subtitle": "'"${DRONE_COMMIT_MESSAGE}"'"
+      },
       "sections": [
         {
+          "widgets": [
+            {
+              "keyValue": {
+                "topLabel": "Build",
+                "content": "'${DRONE_BUILD_NUMBER}'",
+                "onClick": {
+                  "openLink": {
+                    "url": "'"${DRONE_BUILD_LINK}"'"
+                  }
+                }
+              }
+            },
+            {
+              "keyValue": {
+                "topLabel": "Commit",
+                "content": "'"${DRONE_COMMIT_REF}"' by '"${DRONE_COMMIT_AUTHOR}"'"
+              }
+            },
+            {
+              "keyValue": {
+                "topLabel": "Event",
+                "content": "'"${DRONE_BUILD_EVENT}"'"
+              }
+            }
+          ]
+        },
+        {
+          "header": "'"${EVENT_MESS}"'",
        	  "widgets": [
             {
               "image": {
