82320a839603a4e74d4679a81d278af1a47007e8
Francisco Flores <franciscocapipo@gmail.com>
2021-05-25 05:39:22 -0700

    Mejoras al formato (#2)
    * Mueve json a una variable
    
    * Aplica permiso de ejecución al script
    
    * Mueve todo dentro del card
    
    * Muestra referencia y link del evento
    
    * Muestra commit acortado
    
    * Agrega link al commit
    
    * Muestra ícono de estado

--- post-message.sh
+++ post-message.sh
@@ -8,39 +8,104 @@ elif [ ! -z ${PLUGIN_WEBHOOK} ]; then
 	WEBHOOK_URL=$PLUGIN_WEBHOOK
 fi
 
+images_url="https://raw.githubusercontent.com/resuelve/drone-fish/master/images"
+
 if [ "${DRONE_BUILD_STATUS}" == 'failure' ]; then
-	EVENT_MESS="*Uh ...*"
-	EVENT_STATUS=https://raw.githubusercontent.com/resuelve/drone-fish/master/images/ups.png
+	EVENT_MESS="Uh ..."
+	EVENT_STATUS="$images_url/ups.png"
 else
 	if [ "${DRONE_BUILD_EVENT}" == 'pull_request' ]; then
-		EVENT_MESS="*Por favor CR...*"
-		EVENT_STATUS=https://raw.githubusercontent.com/resuelve/drone-fish/master/images/please.png
+		EVENT_MESS="Por favor CR..."
+		EVENT_STATUS="$images_url/please.png"
     else
-		EVENT_MESS="*Listo ...*"
-		EVENT_STATUS=https://raw.githubusercontent.com/resuelve/drone-fish/master/images/success.png
+		EVENT_MESS="Listo ..."
+		EVENT_STATUS="$images_url/success.png"
 	fi
 fi
-curl \
-	-H "Content-Type: aplication/json" \
-	-X POST -d '{"text":" *Drone:* '"${DRONE_REPO}"' '"${DRONE_BUILD_LINK}"'
-  *Commit:* '"${DRONE_COMMIT_REF}"': '"${DRONE_COMMIT_MESSAGE}"'
-  *Author:* '"${DRONE_COMMIT_AUTHOR}"'
-  *Event #:* '"${DRONE_BUILD_NUMBER}"': '"${DRONE_BUILD_EVENT}"'
-  '"${EVENT_MESS}"'",
+
+short_commit="${DRONE_COMMIT:0:8}"
+commit_link="$DRONE_REPO_LINK/commit/$DRONE_COMMIT"
+
+case "$DRONE_BUILD_EVENT" in
+  pull_request)
+    event_ref="#$DRONE_PULL_REQUEST"
+    event_link="$DRONE_REPO_LINK/pull/$DRONE_PULL_REQUEST"
+  ;;
+  tag)
+    event_ref="$DRONE_TAG"
+    event_link="$DRONE_REPO_LINK/releases/tag/$DRONE_TAG"
+  ;;
+  push)
+    event_ref="$short_commit"
+    event_link="$commit_link"
+  ;;
+  promote)
+    event_ref="build ${DRONE_BUILD_PARENT}"
+  ;;
+esac
+
+data='{
   "cards": [
     {
+      "header": {
+        "title": "'"${DRONE_REPO}"'",
+        "subtitle": "'"${DRONE_COMMIT_MESSAGE}"'",
+        "imageUrl": "'"${images_url}"'/status_'"${DRONE_BUILD_STATUS}"'.png"
+      },
       "sections": [
         {
+          "widgets": [
+            {
+              "keyValue": {
+                "topLabel": "Build",
+                "content": "'${DRONE_BUILD_NUMBER}'",
+                "onClick": {
+                  "openLink": {
+                    "url": "'"${DRONE_BUILD_LINK}"'"
+                  }
+                }
+              }
+            },
+            {
+              "keyValue": {
+                "topLabel": "Commit",
+                "content": "'"${short_commit}"' by '"${DRONE_COMMIT_AUTHOR}"'",
+                "onClick": {
+                  "openLink": {
+                    "url": "'"${commit_link}"'"
+                  }
+                }
+              }
+            },
+            {
+              "keyValue": {
+                "topLabel": "Event",
+                "content": "'"${DRONE_BUILD_EVENT}"' '"${event_ref}"'",
+                "onClick": {
+                  "openLink": {
+                    "url": "'"${event_link:-$DRONE_BUILD_LINK}"'"
+                  }
+                }
+              }
+            }
+          ]
+        },
+        {
+          "header": "'"${EVENT_MESS}"'",
        	  "widgets": [
-	    {
-	      "image": {
-	        "imageUrl": "'"${EVENT_STATUS}"'"
-	         }
-	       }
-	    ]
-          }
-        ]
-      }
-    ]
-  }' \
+            {
+              "image": {
+                "imageUrl": "'"${EVENT_STATUS}"'"
+              }
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}'
+
+curl \
+	-H "Content-Type: aplication/json" \
+	-X POST -d "$data" \
 	"${WEBHOOK_URL}"
